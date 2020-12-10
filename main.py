ad9a590066625e90fd9adc8eb9ed06d8fbd93845
Antonia Bustamante <anattolia@gmail.com>
2020-12-10 12:56:21 -0500

    mas

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ api = FastAPI()
 #####
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
-"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-"http://localhost", "http://localhost:8080", "https://cajero-app-antonia.herokuapp.com/"
+"http://localhost.tiangolo.com", 
+"https://localhost.tiangolo.com",
+"http://localhost", 
+"http://localhost:8080", 
+"https://cajero-app-antonia.herokuapp.com"
 ]
 api.add_middleware(
 CORSMiddleware, allow_origins=origins,
